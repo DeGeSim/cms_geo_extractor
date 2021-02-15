08f16d86e1370eccbddd69801bcf59b0c65220f1
mova <mova@users.noreply.github.com>
2021-02-15 10:48:57 +0100

    Add the analyzer

---
+++ EDAnalyzers/GeoExtractor/plugins/GeoExtractor.cc
@@ -0,0 +1,258 @@
+// -*- C++ -*-
+//
+// Package:    EDAnalyzers/GeoExtractor
+// Class:      GeoExtractor
+//
+/**\class GeoExtractor GeoExtractor.cc EDAnalyzers/GeoExtractor/plugins/GeoExtractor.cc
+
+ Description: [one line class summary]
+
+ Implementation:
+     [Notes on implementation]
+*/
+//
+// Original Author:
+//         Created:  Sat, 11 May 2019 13:14:55 GMT
+//
+//
+
+// system include files
+#include <memory>
+
+// user include files
+
+// #include "CommonTools/UtilAlgos/interface/TFileService.h"
+// #include "DataFormats/CaloTowers/interface/CaloTowerDefs.h"
+// #include "DataFormats/Common/interface/MapOfVectors.h"
+// #include "DataFormats/EgammaCandidates/interface/GsfElectron.h"
+// #include "DataFormats/EgammaCandidates/interface/Photon.h"
+#include "DataFormats/FWLite/interface/ESHandle.h"
+#include "DataFormats/ForwardDetId/interface/HGCEEDetId.h"
+#include "DataFormats/ForwardDetId/interface/HGCalDetId.h"
+// #include "DataFormats/GsfTrackReco/interface/GsfTrack.h"
+// #include "DataFormats/HGCalReco/interface/Trackster.h"
+// #include "DataFormats/HGCRecHit/interface/HGCRecHit.h"
+// #include "DataFormats/HepMCCandidate/interface/GenParticle.h"
+// #include "DataFormats/JetReco/interface/PFJet.h"
+// #include "DataFormats/Math/interface/LorentzVector.h"
+#include "DataFormats/ParticleFlowReco/interface/HGCalMultiCluster.h"
+// #include "DataFormats/ParticleFlowReco/interface/PFRecHit.h"
+// #include "DataFormats/RecoCandidate/interface/RecoCandidate.h"
+// #include "DataFormats/TrackReco/interface/Track.h"
+// #include "DataFormats/TrackReco/interface/TrackFwd.h"
+// #include "DataFormats/VertexReco/interface/Vertex.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/Framework/interface/one/EDAnalyzer.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "FWCore/Utilities/interface/InputTag.h"
+#include "Geometry/CaloTopology/interface/HGCalTopology.h"
+#include "Geometry/HGCalGeometry/interface/HGCalGeometry.h"
+#include "Geometry/Records/interface/CaloGeometryRecord.h"
+#include "Geometry/Records/interface/HGCalGeometryRecord.h"
+#include "Geometry/Records/interface/IdealGeometryRecord.h"
+#include "RecoLocalCalo/HGCalRecAlgos/interface/RecHitTools.h"
+// #include "SimDataFormats/CaloAnalysis/interface/CaloParticle.h"
+// #include "SimDataFormats/CaloAnalysis/interface/SimCluster.h"
+// #include "SimDataFormats/CaloHit/interface/PCaloHit.h"
+// #include "SimDataFormats/GeneratorProducts/interface/GenEventInfoProduct.h"
+// #include "SimDataFormats/PileupSummaryInfo/interface/PileupSummaryInfo.h"
+
+#include <Compression.h>
+#include <TH1F.h>
+#include <TH2F.h>
+#include <TMatrixD.h>
+#include <TTree.h>
+#include <TVector2.h>
+#include <TVectorD.h>
+
+#include <fstream>
+#include <iostream>
+// class declaration
+//
+
+// If the analyzer does not use TFileService, please remove
+// the template argument to the base class so the class inherits
+// from  edm::one::EDAnalyzer<>
+// This will improve performance in multithreaded jobs.
+
+class GeoExtractor : public edm::one::EDAnalyzer<edm::one::SharedResources> {
+public:
+  explicit GeoExtractor(const edm::ParameterSet &);
+  ~GeoExtractor();
+
+private:
+  virtual void beginJob() override;
+  virtual void analyze(const edm::Event &, const edm::EventSetup &) override;
+  virtual void endJob() override;
+
+  edm::ESHandle<CaloGeometry> geom;
+  hgcal::RecHitTools recHitTools;
+  std::ofstream myfile;
+};
+
+GeoExtractor::GeoExtractor(const edm::ParameterSet &iConfig) {
+  myfile.open("output.txt");
+  myfile.clear();
+}
+GeoExtractor::~GeoExtractor() { myfile.close(); }
+
+//
+// member functions
+//
+struct Cell {
+  float x;
+  float y;
+  DetID Id;
+  std::vector<DetID> neighbors;
+};
+struct Tile {
+  float middle_x;
+  float middle_y;
+  std::map<DetID, Cell> cells;
+};
+
+struct Wafer {
+  float middle_x;
+  float middle_y;
+  float si_thickness;
+  std::map<DetID, Cell> cells;
+};
+
+struct Layer {
+  float z;
+  std::map<std::pair<int, int>, Wafer> wafers;
+};
+
+struct Subdet {
+  std::map<int, Layer> layers;
+};
+
+struct Det {
+  std::map<int, Subdet> subdetectors;
+};
+
+// ------------ method called for each event  ------------
+void GeoExtractor::analyze(const edm::Event &iEvent, const edm::EventSetup &iSetup) {
+  iSetup.get<CaloGeometryRecord>().get(geom);
+  recHitTools.setGeometry(*(geom.product()));
+
+  //get all valid cells in the geometry, will be filtered later
+  const std::vector<DetId> v_allCellIds = geom->getValidDetIds();
+
+  // Filter the Ids
+  const std::vector<DetId> v_detId;
+  for (int i = 0; i < (int)v_allCellIds.size(); i++) {
+    // Skip IDs from other detector parts
+    if (v_allCellIds[i].det() != DetId::HGCalEE && v_allCellIds[i].det() != DetId::HGCalHSi &&
+        v_allCellIds[i].det() != DetId::HGCalHSc) {
+      continue;
+    }
+    // Todo Position check
+    // If all checks are pass, add the detId to the list of valid Ids.
+    v_detId.push_back(v_allCellIds[i])
+  }
+
+  //Setup the map, that contains all the Structures
+  //Det -> SubDet -> Layer -> Wafer -> Cell
+  //std::map<int, std::map<int, std::vector<DetId>>> m_DetID;
+  std::map<int, Det> m_DetID;
+
+  for (int i = 0; i < (int)v_detId.size(); i++) {
+    DetId cID = v_detId[i];
+    // check angle, forward
+
+    // Logmessage
+    if (i % 100 == 0) {
+      printf("Processing %i", i);
+    }
+
+    // Setup the detector
+    int detectorid = cID.det();
+    // if detector not in map, initialize it;
+    if (m_DetID.find(detectorid) == m_DetID.end()) {
+      m_DetID[detectorid];
+    }
+    Det &detector = m_DetID[detectorid];
+
+    //Setup the subdetector
+    int subdetid = cID.subdetId();
+    if (detector.subdetectors.find(subdetid) == detector.subdetectors.end()) {
+      detector.subdetectors[subdetid];
+    }
+    Subdet &subdet = detector.subdetectors[subdetid];
+
+    //Setup the layer
+    int layerid = recHitTools.getLayer(cID);
+    if (subdet.layers.find(layerid) == subdet.layers.end()) {
+      subdet.layers[layerid];
+      subdet.layers[layerid].z = recHitTools.getPosition(cID)[2];
+    }
+    Layer &layer = subdet.layers[layerid];
+
+    //Setup the Wafer
+    std::pair<int, int> waferid = recHitTools.getWafer(cID);
+    if (layer.wafers.find(waferid) == layer.wafers.end()) {
+      layer.wafers[waferid];
+      // NA
+      // layer.wafers[waferid].middle_x= recHitTools.getPosition(cID)[0];
+      // layer.wafers[waferid].middle_y= recHitTools.getPosition(cID)[1];
+      layer.wafers[waferid].si_thickness = recHitTools.getSiThickIndex(cID);
+    }
+    Wafer &wafer = layer.wafers[waferid];
+
+    //Setup the Cell
+    wafer.cells[cID];
+    Cell &cell = wafer.cells[cID];
+
+    cell.Id = cID;
+    cell.x = recHitTools.getPosition(curcell.Id)[0];
+    cell.y = recHitTools.getPosition(curcell.Id)[1];
+
+    myfile << "\tDet" << cID.det();
+    myfile << "\tSubdet" << subdetid;
+    myfile << "\tLayer" << layerid;
+    myfile << "\tWafer" << waferid;
+    myfile << "\tCellID" << cell.Id;
+    myfile << "\tx" << cell.x;
+    myfile << "\ty" << cell.y;
+    myfile << "\n";
+    n_printed++;
+    if (n_printed > 1000) {
+      return;
+    }
+  }
+}
+
+// ------------ method called once each job just before starting event loop  ------------
+void GeoExtractor::beginJob() {}
+
+// ------------ method called once each job just after ending the event loop  ------------
+void GeoExtractor::endJob() {}
+
+//define this as a plug-in
+DEFINE_FWK_MODULE(GeoExtractor);
+
+// for (int i = 0; i < (int)v_detId.size(); i++) {
+
+//     // Skip IDs from other detector parts
+//     if (v_detId[i].det() < 8) {
+//       continue;
+//     }
+//     // Logmessage
+//     if (i%100 == 0 ) {
+//       printf("Processing %i", i);
+//     }
+//     DetId curId = v_detId[i];
+//     myfile << i << ": Id: " << curId.det();
+//     myfile << "\tSubdetector " << curId.subdetId();
+//     myfile << "\tPosition" << recHitTools.getPosition(curId);
+//     myfile << "\tWafer Id" << recHitTools.getWafer(curId).first << "," << recHitTools.getWafer(curId).second << "\n";
+//     n_printed++;
+//     if (n_printed > 1000) {
+//       return;
+//     }
+//   }
---
+++ EDAnalyzers/GeoExtractor/python/ConfFile_cfg.py
@@ -0,0 +1,73 @@
+import os
+import FWCore.ParameterSet.Config as cms
+
+processName = "Demo"
+
+from Configuration.StandardSequences.Eras import eras
+
+process = cms.Process(processName, eras.Phase2C9)
+
+# import of standard configurations
+process.load("Configuration.StandardSequences.Services_cff")
+process.load("SimGeneral.HepPDTESSource.pythiapdt_cfi")
+process.load("Configuration.EventContent.EventContent_cff")
+process.load("Configuration.StandardSequences.MagneticField_cff")
+process.load("Configuration.StandardSequences.RawToDigi_cff")
+process.load("Configuration.StandardSequences.L1Reco_cff")
+process.load("Configuration.StandardSequences.Reconstruction_cff")
+process.load("Configuration.StandardSequences.RecoSim_cff")
+process.load("Configuration.StandardSequences.EndOfProcess_cff")
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+
+from Configuration.AlCa.GlobalTag import GlobalTag
+
+process.GlobalTag = GlobalTag(process.GlobalTag, "auto:phase2_realistic_T15", "")
+
+process.load("Configuration.Geometry.GeometryExtended2026D49Reco_cff")
+process.load("Configuration.Geometry.GeometryExtended2026D49_cff")
+
+
+############################## Parse arguments ##############################
+
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process.MessageLogger.cerr.threshold = "INFO"
+process.MessageLogger.cerr.FwkReport.reportEvery = 100
+process.MessageLogger.categories.append("Demo")
+process.MessageLogger.cerr.INFO = cms.untracked.PSet(limit=cms.untracked.int32(-1))
+
+process.options = cms.untracked.PSet(wantSummary=cms.untracked.bool(True))
+process.maxEvents = cms.untracked.PSet(input=cms.untracked.int32(1))
+
+
+inputFileNames = cms.untracked.vstring(["file:step1.root"])
+process.source = cms.Source(
+    "PoolSource",
+    fileNames=inputFileNames
+)
+# process.source = cms.Source("EmptySource")
+
+process.analyzer =cms.EDAnalyzer("GeoExtractor")
+process.p = cms.Path(process.analyzer)
+
+process.schedule = cms.Schedule()
+process.schedule.insert(0, process.p)
+
+print "\n"
+print "*" * 50
+print "process.schedule:", process.schedule
+print "*" * 50
+print "\n"
+
+
+process.MessageLogger = cms.Service(
+    "MessageLogger",
+    destinations=cms.untracked.vstring(
+        "cerr",
+    ),
+    cerr=cms.untracked.PSet(
+        # threshold  = cms.untracked.string("ERROR"),
+        DEBUG=cms.untracked.PSet(limit=cms.untracked.int32(0)),
+        WARNING=cms.untracked.PSet(limit=cms.untracked.int32(0)),
+        ERROR=cms.untracked.PSet(limit=cms.untracked.int32(0)),
+    ),
+)
