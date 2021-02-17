13fe3c0d07d52dd889d201ed027812ee01f270bf
mova <mova@users.noreply.github.com>
2021-02-17 15:58:14 +0100

    remove the file dependency

--- EDAnalyzers/GeoExtractor/python/ConfFile_cfg.py
+++ EDAnalyzers/GeoExtractor/python/ConfFile_cfg.py
@@ -39,12 +39,12 @@ process.options = cms.untracked.PSet(wantSummary=cms.untracked.bool(True))
 process.maxEvents = cms.untracked.PSet(input=cms.untracked.int32(1))
 
 
-inputFileNames = cms.untracked.vstring(["file:step1.root"])
-process.source = cms.Source(
-    "PoolSource",
-    fileNames=inputFileNames
-)
-# process.source = cms.Source("EmptySource")
+# inputFileNames = cms.untracked.vstring(["file:step1.root"])
+# process.source = cms.Source(
+#     "PoolSource",
+#     fileNames=inputFileNames
+# )
+process.source = cms.Source("EmptySource")
 process.TFileService = cms.Service("TFileService", fileName=cms.string("DetIdLUT.root"))
 
 process.analyzer =cms.EDAnalyzer("GeoExtractor")
